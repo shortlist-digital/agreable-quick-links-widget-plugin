e46b09f973c531cbe3273d58e2ef5ae4a846a6a3
Leo <leonoragilmour@sky.com>
2017-06-09 16:19:50 +0100

    init

---
+++ agreable-quick-links-widget-plugin.php
@@ -0,0 +1,22 @@
+<?php
+/**
+* @wordpress-plugin
+* Plugin Name: Agreable Quick Links Widget Plugin
+* Plugin URI: http://github.com/shortlist-digital/agreable-quick-links-widget-plugin
+* Description: A plugin for the quick links widget
+* Version: 1.0.0
+* Author: Shortlist Studio
+* Author URI: http://shortlist.studio
+* License: MIT
+*/
+
+require_once __DIR__ . '/../../../../vendor/autoload.php';
+
+class AgreableQuickLinksWidgetPlugin
+{
+    public function __construct()
+    {
+        //
+    }
+}
+new AgreableQuickLinksWidgetPlugin();
---
+++ readme.md
@@ -0,0 +1,2 @@
+# Agreable Quick Links Widget Plugin
+A plugin for the quick links widget
